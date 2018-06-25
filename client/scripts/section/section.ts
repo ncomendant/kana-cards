cbacf44d0ee3a5b202e551a1fccdf05383ab09eb
ncomendant <nicholas.comendant@gmail.com>
2018-06-24 19:41:04 -0700

    Started structuring and styling front end.

---
+++ client/scripts/section/section.ts
@@ -0,0 +1,22 @@
+import { App } from "../app";
+
+declare var $;
+
+export class Section {
+
+    private $section:any;
+    protected app:App;
+
+    public constructor(name:string, app:App) {
+        this.$section = $(`#${name}`);
+        this.app = app;
+    }
+
+    public show():void {
+        this.$section.show();
+    }
+
+    public hide():void {
+        this.$section.hide();
+    }
+}

e6bf7413b2cd422d64101d7b3818ba4f2cbe4be6
ncomendant <nicholas.comendant@gmail.com>
2018-06-24 20:15:36 -0700

    Added backend server code and added express dependency.

--- server/app.ts
+++ server/app.ts
@@ -1,13 +1,45 @@
 import { DatabaseManager } from "./database-manager";
 
+declare function require(moduleName:string):any;
+
+let express = require("express");
+
 export class App {
 
     private db:DatabaseManager;
 
-    public constructor() {
+    public constructor(port:number) {
         this.db = new DatabaseManager();
+        this.initServer(port);
+    }
+
+    private initServer(port:number):void {
+        let app:any = express();
+
+        app.get('/', (req:any, res:any) => {
+            res.send('hello world');
+            //TODO
+        });
+
+        app.get('/login', (req:any, res:any) => {
+            //TODO
+        });
+
+        app.get('/problem', (req:any, res:any) => {
+            //TODO
+        });
+
+        app.get('/search', (req:any, res:any) => {
+            //TODO
+        });
+
+        app.get('/response', (req:any, res:any) => {
+            //TODO
+        });
+
+        app.listen(port);
     }
 
 }
 
-new App();
+new App(3001);
